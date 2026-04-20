3639b08d6bcf9e43f65b34a2b800fffcb097cdad
imblaze4015 <abdulmuhaymenali@gmail.com>
2026-04-20 22:31:10 +0800

    Added logic to parse user_input into individual tokens

--- src/main.s
+++ src/main.s
@@ -26,6 +26,9 @@
 .section .bss
   user_input:
     .space 256
+  
+  argv:
+    .space 80
 
 .section .text
 _start:
@@ -73,63 +76,124 @@ _start:
     je do_exit
   
   not_exit_check:
-  #Demonstrate fork
-  call demo_fork
-  jmp main_loop
-  
-do_exit:
-  #Exit the program
-  mov eax, 60 
-  xor edi, edi
-  syscall
+    #Load the address user_input to rdi and argv tp rsi
+    lea rdi, [rip + user_input]
+    lea rsi, [rip + argv]
 
-demo_fork:
-  #sys_fork
-  push rbx
-  mov eax, 57
-  syscall
+  skip_leading_spaces:
+    #Check if end of string (NULL)? If yes then jmp to finish_tokenize
+    mov al, [rdi]
+    test al, al
+    jz finish_tokenize
+    
+    #If space, Move to the next byte then repeat. Else jump to store_arg
+    cmp al, 32
+    jne store_arg
+    inc rdi
+    jmp skip_leading_spaces
+    
+  store_arg:
+    #Store the argument to arg and move to the next byte
+    mov [rsi], rdi
+    add rsi, 8
   
-  #Check return value
-  test eax, eax
-  jz child_process
-  js fork_error
+  tokenize_loop:
+    #Get the current char
+    mov al, [rdi]
+    
+    #Check if NULL
+    test al, al
+    jz finish_tokenize
+    
+    #Check if space
+    cmp al, 32
+    je handle_space
+    
+    #Move to the next char and jmp to tokenize_loop
+    inc rdi
+    jmp tokenize_loop
+
+  handle_space:
+    #Change space to (NULL) and move to the next bye
+    mov byte ptr [rdi], 0
+    inc rdi
   
-  #Backup rax value to rbx
-  mov rbx, rax
+  handle_multiple_spaces:
+    #Another space? If yes move to the next bye and repeat, Else jump to check_next_word
+    mov al, [rdi]
+    cmp al, 32
+    jne check_next_word
+    inc rdi
+    jmp handle_multiple_spaces
 
-  #Wait4
-  mov rdi, rbx
-  mov eax, 61
-  xor esi, esi
-  xor edx, edx
-  xor r10, r10
-  syscall
 
-  #Print "Parent\n"
-  mov eax, 1
-  mov edi, 1
-  lea rsi, [rip + parent_msg]
-  mov edx, parent_msg_len
-  syscall
+  check_next_word:
+    #End of string? If yes jump to finish_tokenize, Else jump to store_arg
+    test al, al
+    jz finish_tokenize
+    jmp store_arg
+    
+  finish_tokenize:
+    
+    mov qword ptr [rsi], 0
+
+    #Demonstrate fork
+    call demo_fork
+    jmp main_loop
   
-  pop rbx
-  ret 
+  do_exit:
+    #Exit the program
+    mov eax, 60 
+    xor edi, edi
+    syscall
 
-child_process:
-    # Print "Child\n"
+  demo_fork:
+    #sys_fork
+    push rbx
+    mov eax, 57
+    syscall
+  
+    #Check return value
+    test eax, eax
+    jz child_process
+    js fork_error
+  
+    #Backup rax value to rbx
+    mov rbx, rax
+
+    #Wait4
+    mov rdi, rbx
+    mov eax, 61
+    xor esi, esi
+    xor edx, edx
+    xor r10, r10
+    syscall
+
+    #Print "Parent\n"
+    mov eax, 1
+    mov edi, 1
+    lea rsi, [rip + parent_msg]
+    mov edx, parent_msg_len
+    syscall
+  
+    pop rbx
+    ret 
+
+  child_process:
+    #Print "Child\n"
     mov eax, 1
     mov edi, 1
     lea rsi, [rip + child_msg]
     mov edx, child_msg_len
     syscall
 
-    # Child exits (important! or it will return and run main_loop)
+    #Child exits (important! or it will return and run main_loop)
     mov eax, 60
     xor edi, edi
     syscall
 
-fork_error:
-    # Print "Fork failed\n"
+  fork_error:
+    #Print "Fork failed\n"
     mov eax, 1
     mov edi, 1
     lea rsi, [rip + fork_fail_msg]
@@ -139,11 +203,11 @@ fork_error:
     pop rbx
     ret
 
-print_new_line_and_exit:
-  #Print newline before exiting on CTRL + D 
-  mov eax, 1
-  mov edi, 1
-  lea rsi, [rip + newline]
-  mov edx, 1
-  syscall
-  jmp do_exit
+  print_new_line_and_exit:
+    #Print newline before exiting on CTRL + D 
+    mov eax, 1
+    mov edi, 1
+    lea rsi, [rip + newline]
+    mov edx, 1
+    syscall
+    jmp do_exit
